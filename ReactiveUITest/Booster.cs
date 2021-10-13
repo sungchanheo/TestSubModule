286aac1f37f4c15f39968f74b64c2d93522be509
sungchanheo <sungchan.heo@penta-cube.com>
2021-10-13 11:26:27 +0900

    첫 커밋

---
+++ ReactiveUITest/Booster.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+using Unity;
+using Prism.Unity;
+using Prism.Ioc;
+
+namespace ReactiveUITest
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+    }
+}
---
+++ ReactiveUITest/ReactiveTestViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Events;
+using ReactiveUI;
+using Unity;
+
+namespace ReactiveUITest
+{
+    public class ReactiveTestViewModel : ReactiveObject
+    {
+        IUnityContainer _container;
+        IEventAggregator _eventAggregator;
+
+        public ReactiveTestViewModel(IUnityContainer container, IEventAggregator eventAggregator)
+        {
+            _container = container;
+            _eventAggregator = eventAggregator;
+
+            return;
+        }
+    }
+}
